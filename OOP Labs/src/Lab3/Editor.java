eab5378cdd7bf5404164eab5789b8ab2da38bc96
Fridman77 <petru2002.ev@gmail.com>
2021-10-13 22:28:05 +0300

    Finished the task 2 with consones vovels and nr of words and letters. Working on the task 3

--- OOP Labs/src/Lab3/Editor.java
+++ OOP Labs/src/Lab3/Editor.java
@@ -8,7 +8,35 @@ public class Editor { private static String vowelRegex = "aouiey";
         return input.replaceAll("(" + item + ")+", item);
     }
 
-    //
+    public static int countSentences(String input) {
+        String lowerCase = input.toLowerCase();
+        String sanitizedInput = lowerCase.replaceAll("[.?!]+", ".").replaceAll( "[!?]", ".");
+        int flag = (lowerCase.replaceAll("[^a-z]", "").length() > 0)? 1 : 0;
+
+        return Math.max(sanitizedInput.replaceAll("[^.]", "").length(), flag);
+    }
+
+    public static int countWords(String input) {
+        String sanitizedInput = input.toLowerCase().replaceAll("[^a-z-']", " ").strip();
+        sanitizedInput = eliminateRepeatingOccurencies(sanitizedInput, " ");
+
+        return (sanitizedInput.length() > 0)? sanitizedInput.split("\\s").length : 0;
+    }
+
+    public static int countLetters(String input) {
+        String letters = input.toLowerCase().replaceAll("[^a-z]","");
+        return letters.length();
+    }
+
+    public static int countVowels(String input) {
+        String letters = input.toLowerCase().replaceAll("[^a-z]", "");
+        return letters.replaceAll("[^" + vowelRegex + "]", "").length();
+    }
+
+    public static int countConsonants(String input) {
+        String letters = input.toLowerCase().replaceAll("[^a-z]", "");
+        return letters.replaceAll("[" + vowelRegex + "]", "").length();
+    }
 
     private static HashMap<String, Integer> getWordHashMap(String input) {
         input = input.toLowerCase().replaceAll("[^a-z\\s]", "");
--- OOP Labs/src/Lab3/Main.java
+++ OOP Labs/src/Lab3/Main.java
@@ -6,7 +6,11 @@ public class Main
     public static void main(String[] args) {
         String rawText = "Satisfied conveying an dependent contented he gentleman agreeable do be. Warrant private blushes removed an in equally totally if. Delivered dejection necessary objection do mr prevailed. Mr feeling do chiefly cordial in do. Water timed folly right aware if oh truth. Imprudence attachment him his for sympathize. Large above be to means. Dashwood do provided stronger is. But discretion frequently sir the she instrument unaffected admiration everything.";
 
-        System.out.println("The longest word is:" +Lab3.Editor.getLongestWord(rawText));
-
+        System.out.println("The longest word is: " +Lab3.Editor.getLongestWord(rawText));
+        System.out.println("Nr. of sentences are: " +Lab3.Editor.countSentences(rawText));
+        System.out.println("Nr. of words are: " +Lab3.Editor.countWords(rawText));
+        System.out.println("Nr. of Letters : " +Lab3.Editor.countLetters(rawText));
+        System.out.println("Nr. of Vovels: " +Lab3.Editor.countVowels(rawText));
+        System.out.println("Nr. of Consones: " +Lab3.Editor.countConsonants(rawText));
     }
 }
